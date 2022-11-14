629e9cd9707167e7fd1bb01bf57f72c733f4ed72
greengiant83 <greengiant83@hotmail.com>
2022-11-14 15:12:01 -0600

    Added module

--- Runtime/Interaction System/XRUIManager.cs
+++ Runtime/Interaction System/XRUIManager.cs
@@ -1,30 +1,30 @@
-using Autohand;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using Autohand;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class XRUIManager : IUIManager
-{
-    public void MakeGrabbable(GameObject Item)
-    {
-        Rigidbody body = Item.GetComponent<Rigidbody>();
-        if (body == null)
-        {
-            body = Item.AddComponent<Rigidbody>();
-            body.useGravity = false;
-            body.drag = 0.2f;
-            body.angularDrag = 0.2f;
-            //checkPhysicsMaterials(Item); //TODO: Would be nice to assign a default physics material so things have some bounce
-        }
+//public class XRUIManager : IUIManager
+//{
+//    public void MakeGrabbable(GameObject Item)
+//    {
+//        Rigidbody body = Item.GetComponent<Rigidbody>();
+//        if (body == null)
+//        {
+//            body = Item.AddComponent<Rigidbody>();
+//            body.useGravity = false;
+//            body.drag = 0.2f;
+//            body.angularDrag = 0.2f;
+//            //checkPhysicsMaterials(Item); //TODO: Would be nice to assign a default physics material so things have some bounce
+//        }
 
-        Grabbable grabbable = Item.GetComponent<Grabbable>();
-        if (grabbable == null)
-        {
-            grabbable = Item.AddComponent<Grabbable>();
-            grabbable.parentOnGrab = false;
+//        Grabbable grabbable = Item.GetComponent<Grabbable>();
+//        if (grabbable == null)
+//        {
+//            grabbable = Item.AddComponent<Grabbable>();
+//            grabbable.parentOnGrab = false;
 
-            Item.AddComponent<DistanceGrabbable>();
-            Item.AddComponent<XRGrabbable>();
-        }
-    }
-}
+//            Item.AddComponent<DistanceGrabbable>();
+//            Item.AddComponent<XRGrabbable>();
+//        }
+//    }
+//}
---
+++ Runtime/XrUIModule.cs
@@ -0,0 +1,34 @@
+﻿using Autohand;
+using System.Collections;
+using UnityEngine;
+
+public static class XrUIModule
+{
+    public static void Initialize()
+    {
+        AppControllerBase.Instance.UIManager.OnMakeGrabbable += UIManager_OnMakeGrabbable;
+    }
+
+    private static void UIManager_OnMakeGrabbable(GameObject Item)
+    {
+        Rigidbody body = Item.GetComponent<Rigidbody>();
+        if (body == null)
+        {
+            body = Item.AddComponent<Rigidbody>();
+            body.useGravity = false;
+            body.drag = 0.2f;
+            body.angularDrag = 0.2f;
+            //checkPhysicsMaterials(Item); //TODO: Would be nice to assign a default physics material so things have some bounce
+        }
+
+        Grabbable grabbable = Item.GetComponent<Grabbable>();
+        if (grabbable == null)
+        {
+            grabbable = Item.AddComponent<Grabbable>();
+            grabbable.parentOnGrab = false;
+
+            Item.AddComponent<DistanceGrabbable>();
+            Item.AddComponent<XRGrabbable>();
+        }
+    }
+}
