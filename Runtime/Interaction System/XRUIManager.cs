e4b0333ad01735092002761b1039690d3763657a
greengiant83 <greengiant83@hotmail.com>
2022-11-14 11:56:45 -0600

    ...

--- Runtime/Interaction System/XRUIManager.cs
+++ Runtime/Interaction System/XRUIManager.cs
@@ -7,16 +7,24 @@ public class XRUIManager : IUIManager
 {
     public void MakeGrabbable(GameObject Item)
     {
-        var body = Item.AddComponent<Rigidbody>();
-        body.useGravity = false;
-        body.drag = 0.2f;
-        body.angularDrag = 0.2f;
-        //checkPhysicsMaterials(Item); //TODO: Would be nice to assign a default physics material so things have some bounce
+        Rigidbody body = Item.GetComponent<Rigidbody>();
+        if (body == null)
+        {
+            body = Item.AddComponent<Rigidbody>();
+            body.useGravity = false;
+            body.drag = 0.2f;
+            body.angularDrag = 0.2f;
+            //checkPhysicsMaterials(Item); //TODO: Would be nice to assign a default physics material so things have some bounce
+        }
 
-        var grabbable = Item.AddComponent<Grabbable>();
-        grabbable.parentOnGrab = false;
+        Grabbable grabbable = Item.GetComponent<Grabbable>();
+        if (grabbable == null)
+        {
+            grabbable = Item.AddComponent<Grabbable>();
+            grabbable.parentOnGrab = false;
 
-        Item.AddComponent<DistanceGrabbable>();
-        Item.AddComponent<XRGrabbable>();
+            Item.AddComponent<DistanceGrabbable>();
+            Item.AddComponent<XRGrabbable>();
+        }
     }
 }
