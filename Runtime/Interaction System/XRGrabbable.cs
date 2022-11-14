dd2da7a58247fc0fe51f02a2db27ee2d29e560bd
greengiant83 <greengiant83@hotmail.com>
2022-11-14 17:01:39 -0600

    ...

--- Runtime/Interaction System/XRGrabbable.cs
+++ Runtime/Interaction System/XRGrabbable.cs
@@ -13,8 +13,16 @@ public class XRGrabbable : MonoBehaviour, IGrabbable
     private void Awake()
     {
         grabbable = GetComponent<Grabbable>();
+        grabbable.OnBeforeGrabEvent = Grabbable_OnBeforeGrab; //NOTE: Autohands exposes this as a single delegate NOT an event, so only one thing can handle this event :(
         grabbable.onGrab.AddListener(Grabbable_OnGrab);
         grabbable.onRelease.AddListener(Grabbable_OnRelease);
+        
+    }
+
+    private void Grabbable_OnBeforeGrab(Hand hand, Grabbable grabbable)
+    {
+        var grabber = hand.GetComponent<IGrabber>();
+        this.OnBeforeGrab?.Invoke(grabber, this);
     }
 
     private void Grabbable_OnGrab(Hand hand, Grabbable grabbable)
